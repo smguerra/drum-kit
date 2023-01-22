e7b55e66302efc78204d39999021da2e066ac33a
Milena <milenagg77@hotmail.com>
2023-01-22 11:39:58 +0000

    added function buttonanimation

--- assets/script/index.js
+++ assets/script/index.js
@@ -66,7 +66,17 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     }
   }
 
+  function buttonAnimation(currentKey) {
 
+    var activeButton = document.querySelector("." + currentKey);
+  
+    activeButton.classList.add("pressed");
+  
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+  
+  }
 //  var audio = new Audio("/sounds/tom-1.mp3");
 //  audio.play();
 //this.style.color = "white";
