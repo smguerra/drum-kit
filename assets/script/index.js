ef96bfdcd17db3374e62d973bd74cd5844fa4fe0
Milena <milenagg77@hotmail.com>
2023-01-22 11:14:24 +0000

    created code for sound and animation

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,5 +2,21 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("hola")});
-    }
+        
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+    });
+
+ }
+
+ 
+
+
+//  var audio = new Audio("/sounds/tom-1.mp3");
+//  audio.play();
+//this.style.color = "white";
+        
